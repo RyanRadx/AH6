03fd6601454864cf3c4e53d8909ac62158d82ab6
Radwanski <ryan.radwanski@fiserv.com>
2023-11-01 16:41:08 -0400

    JOptionPane + StringBuilder solush

--- Solution.java
+++ Solution.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import javax.swing.JOptionPane;
 
 import javax.xml.transform.Source;
 
@@ -13,12 +14,25 @@ class Node<T> {
 }
 
 class Solution {
-  public static List<String> linkedListValues(Node<String> head) {
-    // todo
-    return;
+  public static String linkedListValues(Node<String> head) {
+    StringBuilder out = new StringBuilder();
+    //out = out.append(head.val);
+
+    // System.println(out.toString());
+    // String ex = head.val;
+
+    // System.println(ex);
+
+    while (head.next != null){
+      out.append(head.val);
+      head = head.next;
+    }
+    out.append (head.val);
+    String output = out.toString();
+    return output;
   }
 
-  public static void main() {
+  public static void main(String [] args) {
     Node<String> a = new Node<>("a");
     Node<String> b = new Node<>("b");
     Node<String> c = new Node<>("c");
@@ -28,8 +42,8 @@ class Solution {
     c.next = d;
 
     // a -> b -> c -> d
-
-    Solution.linkedListValues(a); 
+    // Solution.linkedListValues(a);
+    JOptionPane.showMessageDialog(null, Solution.linkedListValues(a));
     // -> [ "a", "b", "c", "d" ]
   }
 }
